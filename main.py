0d9aa0c83efe865ee6c702c1e6b4811cb8499a52
Pavel Cerny <cerny.pav@gmail.com>
2015-04-11 21:06:49 +0200

    validace, zda funguje server spravne, bez zpracovani dat

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ __author__ = 'Pavel'
 
 from http.server import HTTPServer, BaseHTTPRequestHandler
 import threading
+import time
 
 threadCounter = 0
 postThreads = []
@@ -17,33 +18,35 @@ class postThread (threading.Thread):
     def __init__(self, request):
         threading.Thread.__init__(self)
     def run(self):
-        global words
-        if self.request.path == "/osp/myserver/data":
-            length = int(self.request.headers.get('Content-Length'))
-            text = self.request.rfile.read(length).decode("utf-8")
-            for word in text.split():
-                words[word] = 1
-            self.request.send_response(204) # No Content
-            self.request.end_headers()
-        else:
-            self.request.send_response(404)
-            self.request.end_headers()
+        print ("post")
+        # global words
+        # if self.request.path == "/osp/myserver/data":
+        #     length = int(self.request.headers.get('Content-Length'))
+        #     text = self.request.rfile.read(length).decode("utf-8")
+        #     for word in text.split():
+        #         words[word] = 1
+        #     self.request.send_response(204) # No Content
+        #     self.request.end_headers()
+        # else:
+        #     self.request.send_response(404)
+        #     self.request.end_headers()
 
 
 class getThread (threading.Thread):
     def __init__(self, request):
         threading.Thread.__init__(self)
     def run(self):
-        global words
-        if self.request.path == "/osp/myserver/count":
-            self.request.send_response(200)
-            self.request.send_header("Content-type", "text/plain")
-            self.request.end_headers()
-            self.request.wfile.write(str(len(words)).encode())
-            words = {}
-        else:
-            self.request.send_response(404)
-            self.request.end_headers()
+        print ("get")
+        # global words
+        # if self.request.path == "/osp/myserver/count":
+        #     self.request.send_response(200)
+        #     self.request.send_header("Content-type", "text/plain")
+        #     self.request.end_headers()
+        #     self.request.wfile.write(str(len(words)).encode())
+        #     words = {}
+        # else:
+        #     self.request.send_response(404)
+        #     self.request.end_headers()
 
 
 
@@ -51,14 +54,14 @@ class getThread (threading.Thread):
 class OSPHTTPHandler(BaseHTTPRequestHandler):
     def do_POST(self):
         global postThreads
-        t = postThread
+        t = postThread(self)
         postThreads.append(t)
         t.start()
 
 
     def do_GET(self):
         global postThreads, getT
-        getT = getThread
+        getT = getThread(self)
         for t in postThreads:
             t.join()
         postThreads=[]
