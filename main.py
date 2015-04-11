6c5027fa7992b123bce058741c8cee2031c02ba8
Pavel Cerny <cerny.pav@gmail.com>
2015-04-11 20:49:37 +0200

    threads

--- main.py
+++ main.py
@@ -4,34 +4,74 @@ __author__ = 'Pavel'
 from http.server import HTTPServer, BaseHTTPRequestHandler
 import threading
 
+threadCounter = 0
+postThreads = []
+getT = None
+
 words = {}
+getIsWaiting = 0
+getIsRunning = threading.Lock()
 
-class OSPHTTPHandler(BaseHTTPRequestHandler,threading.Thread):
-    def do_POST(self):
+
+class postThread (threading.Thread):
+    def __init__(self, request):
+        threading.Thread.__init__(self)
+    def run(self):
         global words
-        if self.path == "/osp/myserver/data":
-            length = int(self.headers.get('Content-Length'))
-            text = self.rfile.read(length).decode("utf-8")
+        if self.request.path == "/osp/myserver/data":
+            length = int(self.request.headers.get('Content-Length'))
+            text = self.request.rfile.read(length).decode("utf-8")
             for word in text.split():
-                words[word] = 1;
-            self.send_response(204) # No Content
-            self.end_headers()
+                words[word] = 1
+            self.request.send_response(204) # No Content
+            self.request.end_headers()
         else:
-            self.send_response(404)
-            self.end_headers()
+            self.request.send_response(404)
+            self.request.end_headers()
 
-    def do_GET(self):
+
+class getThread (threading.Thread):
+    def __init__(self, request):
+        threading.Thread.__init__(self)
+    def run(self):
         global words
-        if self.path == "/osp/myserver/count":
-            self.send_response(200)
-            self.send_header("Content-type", "text/plain")
-            self.end_headers()
-            self.wfile.write(str(len(words)).encode())
+        if self.request.path == "/osp/myserver/count":
+            self.request.send_response(200)
+            self.request.send_header("Content-type", "text/plain")
+            self.request.end_headers()
+            self.request.wfile.write(str(len(words)).encode())
             words = {}
         else:
-            self.send_response(404)
-            self.end_headers()
+            self.request.send_response(404)
+            self.request.end_headers()
+
+
+
+
+class OSPHTTPHandler(BaseHTTPRequestHandler):
+    def do_POST(self):
+        global postThreads
+        t = postThread
+        postThreads.append(t)
+        t.start()
+
+
+    def do_GET(self):
+        global postThreads, getT
+        getT = getThread
+        for t in postThreads:
+            t.join()
+        postThreads=[]
+        getT.run()
+        getT=None
 
 httpd = HTTPServer(('', 8000), OSPHTTPHandler)
 print("Listening on port", httpd.server_port)
-httpd.serve_forever()
+#httpd.serve_forever()
+
+#process post requests simultaneously, but wait when get request
+while 1:
+    if getT is None:
+        httpd.handle_request()
+    else:
+        getT.join()
